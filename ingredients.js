785f2e73494c654e8da19104614576acc3776e0d
gavinswan <gavtswan@gmail.com>
2021-01-15 04:38:29 +0000

    looping over arrays to list baking ingredients

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+console.log(ingredients.length);
+
+for (i = 7; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
